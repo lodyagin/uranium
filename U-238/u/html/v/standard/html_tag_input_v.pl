304c905ae740008d93d21c1ef3b15061004966d3
Sergei Lodyagin <serg@kogorta.dp.ua>
2011-10-11 00:10:01 +0300

    Add v option to ixpath

---
+++ U-238/u/html/v/standard/html_tag_input_v.pl
@@ -0,0 +1,59 @@
+%  This file is a part of Uranium, a general-purpose functional
+%  test platform.
+%
+%  Copyright (C) 2011  Sergei Lodyagin
+%
+%  This library is free software; you can redistribute it and/or
+%  modify it under the terms of the GNU Lesser General Public
+%  License as published by the Free Software Foundation; either
+%  version 2.1 of the License, or (at your option) any later
+%  version.
+%
+%  This library is distributed in the hope that it will be
+%  useful, but WITHOUT ANY WARRANTY; without even the implied
+%  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+%  PURPOSE.  See the GNU Lesser General Public License for more
+%  details.
+%
+%  You should have received a copy of the GNU Lesser General
+%  Public License along with this library; if not, write to the
+%  Free Software Foundation, Inc., 51 Franklin Street, Fifth
+%  Floor, Boston, MA 02110-1301 USA
+%
+%  e-mail: lodyagin@gmail.com
+%  post:   49017 Ukraine, Dnepropetrovsk per. Kamenski, 6
+
+:- module(html_tag_input_v, []).
+
+:- use_module(u(v)).
+
+new_class(html_tag_input_v, html_tag_v, []).
+
+new_class(html_input_attrs_v, html_attrs_v,
+          [
+           '.name',
+           '.type',
+           '.value',
+           '.checked',
+           '.maxlength',
+           '.src',
+           '.alt',
+           '.accept',
+           '.disabled',
+           '.readonly',
+           '.accesskey',
+           '.tabindex'
+          ]).
+
+downcast(html_tag_v, html_tag_input_v, From, To) :-
+
+   obj_field(From, html_tag_attrs, Old_Attrs),
+   obj_downcast(Old_Attrs, html_input_attrs_v, New_Attrs),
+   obj_field(To, html_input_attrs_v, New_Attrs).
+
+downcast(html_attrs_v, html_input_attrs_v, From, To) :-
+
+   obj_field(From, '.@bulk', Old_Bulk),
+   construct_html_attrs(html_input_attrs_v, Old_Bulk, To).
+   % other attributes are copied by Uranium
+   
