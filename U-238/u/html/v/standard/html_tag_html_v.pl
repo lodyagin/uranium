2654c5feb6030285279e3a6ba04c04b5a65d7a75
Sergei Lodyagin <serg@kogorta.dp.ua>
2011-10-13 02:39:04 +0300

    Fix html_tag_html class & dc rules

--- U-238/u/html/v/standard/html_tag_html_v.pl
+++ U-238/u/html/v/standard/html_tag_html_v.pl
@@ -32,13 +32,13 @@ new_class(html_tag_html_v, html_tag_v, []).
 
 'html_tag_html_v?'(Obj, class, Class) :-
 
-   (  ixpath(/body, Obj, _)
+   (  ixpath(child::body, Obj, _)
    -> Class = page_v
    ;  true ).
 
 
 downcast(html_tag_html_v, page_v, From, To) :-
 
-   (  ixpath(/head/title, From, Title)
+   (  ixpath(child::head/child::title, From, Title)
    -> obj_field(To, title, Title)
    ;  true ).
