973ba48be659d46ad02e6ae907801670a90418f8
Sergei Lodyagin <serg@kogorta.dp.ua>
2011-10-19 13:41:21 +0300

    Add class_fields/0

--- U-238/u/class_diagram.pl
+++ U-238/u/class_diagram.pl
@@ -24,13 +24,16 @@
 %  post:   49017 Ukraine, Dnepropetrovsk per. Kamenski, 6
 
 :- module(class_diagram,
-          [class_diagram/0,
+          [all_fields/0,
+           class_diagram/0,
            class_fields/0
           ]).
 
 :- use_module(u(v)).
 :- use_module(u(internal/objects_i)).
 :- use_module(library(ugraphs)).
+
+:- dynamic field/3. %Name, Class, Prefix_Class
 
 class_diagram :-
 
@@ -85,3 +88,51 @@ class_fields2(Class) :-
    class_fields_new(Class, Fields),
    maplist(format('~a|'), Fields),
    write('|').
+
+
+all_fields :-
+
+   build_fields_db,
+   setof(A/C, B^fields(A, B, C), List),
+   print_fields(List).
+
+print_fields(['#'/_|T]) :- !,
+   print_fields(T).
+
+print_fields([Name/Prefix|T]) :-
+   var(Prefix), !,
+   writeln(Name),
+   print_fields(T).
+   
+print_fields([Name/Prefix|T]) :-
+   format('  ~a:~a\n', [Prefix, Name]),
+   print_fields(T).
+   
+build_fields_db :-
+
+   retractall(fields(_, _, _)),
+   assert(fields('#', '#', '#')),
+   
+   class_graph(Graph),
+   top_sort(Graph, Order),
+   (  member(Class, Order),
+      add_fields_to_db(Class),
+      fail ; true ).
+   
+add_fields_to_db(Class) :-
+
+   class_fields_new(Class, Fields),
+   add_fields_to_db(Class, Fields).
+
+add_fields_to_db(_, []) :- !.
+
+add_fields_to_db(Class, [Field|T]) :-
+
+   (  fields(Field, _, _)
+   -> (  retract(fields(Field, Orig_Class, _)),
+         assert(fields(Field, Orig_Class, Orig_Class)),
+         fail ; true ),
+      assert(fields(Field, Class, Class))
+   ;  assert(fields(Field, Class, _))
+   ),
+   add_fields_to_db(Class, T).
