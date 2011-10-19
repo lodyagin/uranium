79848eb4d28c7375e510d72f3892923b3885fcc1
Sergei Lodyagin <serg@kogorta.dp.ua>
2011-10-19 11:48:20 +0300

    Add html_tag_label_v

--- U-238/u/html/v/standard/html_tag_v.pl
+++ U-238/u/html/v/standard/html_tag_v.pl
@@ -40,7 +40,9 @@ new_class(html_tag_v, html_piece_v,
    ).
 
 downcast(html_tag_v, html_tag_input_v, From, To) :-
+   gen_html_tag_downcast(From, To).
 
+downcast(html_tag_v, html_tag_label_v, From, To) :-
    gen_html_tag_downcast(From, To).
 
 unify_html_attrs(_, [], Rest, Rest) :- !. 
