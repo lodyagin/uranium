e27586d0346ac62ed7a065fb018f1675526d05cd
Sergei Lodyagin <serg@kogorta.dp.ua>
2012-02-18 16:48:17 +0200

    Add lcq passing

---
+++ U-238/u/rand/randseq.pl
@@ -0,0 +1,41 @@
+:- module(randseq,
+          [randseq/2,
+           lcq_gnu/1]
+         ).
+
+:- use_module(library(clpfd)).
+
+lcq_gnu(lcq(1103515245, 12345, 4294967296)).
+
+randseq(LCQ, L) :-
+
+   LCQ = lcq(_, _, M),
+   randseq(LCQ, L, S),
+   Max is M - 1,
+   S ins 0..Max,
+   label(S).
+
+randseq(_, [], []) :- !.
+
+randseq(_, [X], [Seed]) :-
+
+   norm(Seed, X), !.
+
+randseq(LCQ, [X|L], [Seed|S]) :-
+
+   S = [Seed1|_],
+   norm(Seed, X),
+   lcq(LCQ, Seed, Seed1),
+   randseq(LCQ, L, S).
+
+lcq(lcq(A, C, M), X0, X) :-
+
+   X #= (A * X0 + C) mod M.
+
+% map Seed to X domain
+norm(Seed, X) :-
+   fd_inf(X, Min),
+   fd_sup(X, Max),
+   X #= Seed mod (Max - Min + 1) + Min.
+
+   
