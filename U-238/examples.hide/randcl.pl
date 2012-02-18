f9feb20f2578c333a5bbfd973a8dc79eb6d7ba07
Sergei Lodyagin <serg@kogorta.dp.ua>
2012-02-18 16:01:24 +0200

    Add normalization.

--- U-238/examples.hide/randcl.pl
+++ U-238/examples.hide/randcl.pl
@@ -1,27 +1,31 @@
+% example:
+% length(L, 20), L ins 32..126, randseq(32, 126, L, S), S ins 0..4294967295, all_different(L), label(S), writeln(L).
+
 :- use_module(library(clpfd)).
 
-randseq(L) :-
+randseq(Min .. Max, L) :-
 
-   randseq(L, _).
+   randseq(Min, Max, L, _).
 
-randseq([], []) :- !.
+randseq(_, _, [], []) :- !.
 
-randseq([Seed], [Seed]) :- !.
+randseq(Min, Max, [X], [Seed]) :-
 
-randseq([X|L], [Seed|S]) :-
+   norm(Min, Max, Seed, X), !.
 
-   norm(Seed, X),
+randseq(Min, Max, [X|L], [Seed|S]) :-
+
+   S = [Seed1|_],
+   norm(Min, Max, Seed, X),
    lcq(Seed, Seed1),
-   randseq(Seed1, L, S).
+   randseq(Min, Max, L, S).
 
 lcq(X0, X) :-
 
    X #= (1103515245 * X0 + 12345) mod 2 ^ 32.
 
 % map Seed to X domain
-norm(Seed, X) :-
-   fd_inf(X, Min),
-   fd_sup(X, Max),
+norm(Min, Max, Seed, X) :-
    X #= Seed mod (Max - Min + 1) + Min.
 
    
