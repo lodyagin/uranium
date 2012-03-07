4ae3d63ec9d8bca8f4c08e2692baa0eb8c14124f
Sergei Lodyagin <serg@kogorta.dp.ua>
2012-03-07 19:24:27 +0200

    Add lambda/3,5

--- U-238/u/util/lambda.pl
+++ U-238/u/util/lambda.pl
@@ -2,29 +2,57 @@
 
 :- module(lambda,
 	  [arg_reorder/4,
-	   arg_reorder/5
+	   arg_reorder/5,
+           lambda/3,
+           lambda/5
 	  ]).
 
 :- use_module(u(ur_lists)).
 
 :- meta_predicate arg_reorder(2, +, ?, ?).
 :- meta_predicate arg_reorder(3, +, ?, ?, ?).
+:- meta_predicate lambda(+, 0, +).
+:- meta_predicate lambda(+, +, 0, +, +).
 
 % Argument reordering
 
 arg_reorder(Goal, Order, Arg1, Arg2) :-
 
-	extract_by_key_order(Order, [1 - Arg1, 2 - Arg2], L1),
-	pairs_values(L1, Reordered_Args),
-	apply(Goal, Reordered_Args).
+   extract_by_key_order(Order, [1 - Arg1, 2 - Arg2], L1),
+   pairs_values(L1, Reordered_Args),
+   apply(Goal, Reordered_Args).
 
 arg_reorder(Goal, Order, Arg1, Arg2, Arg3) :-
 
-	extract_by_key_order(Order,
-			     [1-Arg1, 2-Arg2, 3-Arg3],
-			     L1),
-	pairs_values(L1, Reordered_Args),
-	apply(Goal, Reordered_Args).
+   extract_by_key_order(Order,
+                        [1-Arg1, 2-Arg2, 3-Arg3],
+                        L1),
+   pairs_values(L1, Reordered_Args),
+   apply(Goal, Reordered_Args).
 
+% Lambda adapters
+
+lambda(X1, Pred, X) :-
+
+   catch(ignore((X1 = X,
+                 (  call(Pred) -> true
+                 ;  throw(fail)
+                 ),
+                 fail
+                )),
+         _,
+         fail).
+         
+lambda(X1, Y1, Pred, X, Y) :-
+
+   catch(ignore((X1 = X, Y1 = Y,
+                 (  call(Pred) -> true
+                 ;  throw(fail)
+                 ),
+                 fail
+                )),
+         _,
+         fail).
+         
 
 
