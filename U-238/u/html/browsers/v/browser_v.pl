bb4f909167b89d2cb207bee18ed7a10c483399c9
Sergei Lodyagin <lodyagin@gmail.com>
2012-04-17 15:40:26 +0300

    Add Keep-Alive: 115 header for firefox

--- U-238/u/html/browsers/v/browser_v.pl
+++ U-238/u/html/browsers/v/browser_v.pl
@@ -53,18 +53,20 @@ new_class(firefox_browser_v, browser_v, []).
 % TODO ignore version, always 3.6.26
 'firefox_browser_v?'(_, headers, Headers) :-
 
-   obj_construct(http_request_headers_v,
+   obj_construct(http_experimental_1_0_request_headers_v,
                  [user_agent,
                   accept,
                   accept_language,
                   accept_encoding,
-                  accept_charset
+                  accept_charset,
+                  keep_alive
                  ],
                  ['Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.26) Gecko/20120202 Iceweasel/3.6.26 (like Firefox/3.6.26)',
 		  'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                   'en-us,en;q=0.5',
                   'gzip,deflate',
-                  'ISO-8859-1,utf-8;q=0.7,*;q=0.7'
-                 ], Headers).
+                  'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+                  115
+                  ], Headers).
 
 
