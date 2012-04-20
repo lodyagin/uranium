cdd328f203cbbba36ce897da700942befadc601d
Sergei Lodyagin <lodyagin@gmail.com>
2012-04-20 13:39:42 +0300

    http_request_url -> www_address in html_page_parse.pl

--- U-238/u/parser/html/html_page_parse.pl
+++ U-238/u/parser/html/html_page_parse.pl
@@ -1,24 +1,30 @@
-%% This file is a part of Uranium, a general-purpose functional test platform.
-%% Copyright (C) 2011  Sergei Lodyagin
-%%
-%% This library is free software; you can redistribute it and/or
-%% modify it under the terms of the GNU Lesser General Public
-%% License as published by the Free Software Foundation; either
-%% version 2.1 of the License, or (at your option) any later version.
-%%
-%% This library is distributed in the hope that it will be useful,
-%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-%% Lesser General Public License for more details.
-
-%% You should have received a copy of the GNU Lesser General Public
-%% License along with this library; if not, write to the Free Software
-%% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-%%
-%% e-mail: lodyagin@gmail.com
-%% post:   49017 Ukraine, Dnepropetrovsk per. Kamenski, 6
-%% -------------------------------------------------------------------------------
-%%
+% -*- fill-column: 65; -*-
+%
+%  This file is a part of Uranium, a general-purpose
+%  functional test platform.
+%
+%  Copyright (C) 2011, Sergei Lodyagin
+%  Copyright (C) 2012, Kogorta OOO Ltd
+%
+%  This library is free software; you can redistribute it
+%  and/or modify it under the terms of the GNU Lesser
+%  General Public License as published by the Free
+%  Software Foundation; either version 2.1 of the License,
+%  or (at your option) any later version.
+%
+%  This library is distributed in the hope that it will be
+%  useful, but WITHOUT ANY WARRANTY; without even the
+%  implied warranty of MERCHANTABILITY or FITNESS FOR A
+%  PARTICULAR PURPOSE.  See the GNU Lesser General Public
+%  License for more details.
+%
+%  You should have received a copy of the GNU Lesser
+%  General Public License along with this library; if not,
+%  write to the Free Software Foundation, Inc., 51
+%  Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+%
+%  e-mail: lodyagin@gmail.com
+%  post:   49017 Ukraine, Dnepropetrovsk per. Kamenski, 6
 
 :- module(html_page_parse,
           [html_page_parse/3
@@ -64,10 +70,8 @@ extract_element(Page, Class, Object) :-
   debug(html_page_parse, 'Call ~a for ~p', [Pred, Data]),
   call(Pred, Data, Object1),
 
-  named_args_unify(Page, [http_request_url, timestamp],
-                   [Obj_Url, Obj_Timestamp]),
-  named_args_unify(Object1, [http_request_url, timestamp],
-                   [Obj_Url, Obj_Timestamp]),
+  obj_field(Page, www_address, WWW_Address),
+  obj_field(Object1, www_address, WWW_Address),
 
   obj_downcast(Object1, Object),
 
