866b9fd038eaa7e51a359bfcc436d6a0086e26a8
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-09-04 15:24:07 +0000

    action/find wip changes

--- U-238/u/action/find.pl
+++ U-238/u/action/find.pl
@@ -26,7 +26,7 @@
 %  e-mail: lodyagin@gmail.com
 %  post:   49017 Ukraine, Dnepropetrovsk per. Kamenski, 6
 
-:- module(find, [find/5]).
+:- module(find, [find/6]).
 
 :- use_module(u(ur_words)).
 :- use_module(u(ur_lists)).
@@ -34,26 +34,30 @@
 :- use_module(u(v)).
 :- use_module(u(tc_support/load_action_module)).
 :- use_module(u(logging)).
+:- use_module(u(internal/check_arg)).
 
-find(SAT, Actor, Object_Type, Prop_List, DB_Key) :-
+%% find(+DB_Key, +SAT, +Object_Type, +Prop_List, +Actor0, -Actor) is det.
+%
+% Find all objects of the type _|Object_Type|__v in SAT.
+% 
+% The search is 2 stage process:
+% 1. Call the ('find_all_' + Object_type)/3 pred from the module action/
 
-  atom(DB_Key), !,
+find(DB_Key, SAT, Object_Type, Prop_List, Actor0, Actor) :-
+  db_key_is_valid(DB_Key), !,
+  check_db_key(DB_Key, context(find/6, _)),
+
   change_plural(Object_Type, Plural),
-  %atom_concat('find.', Plural, DB_Key),
   decode_prop_list(Prop_List, Field_Names, Field_Values),
-  %db_clear(DB_Key),
   atom_concat('find_all_', Plural, Find_All_Pred),
-  (Object_Type = 'user' -> Module_Name = user_man;
-   Module_Name = Object_Type
-  ),
+  Module_Name = Object_Type,
   load_action_module(SAT, Module_Name, Full_Module_Name),
-  call(Full_Module_Name:Find_All_Pred, DB_Key, Actor),
+  call(Full_Module_Name:Find_All_Pred, DB_Key, Actor0, Actor),
   filter_on_db(DB_Key, throw, Field_Names, Field_Values),
 
   write_log('find iteration 1:', [logger(dump_db), lf(1)]),
-  dump_db(DB_Key). % logging
+  dump_db(DB_Key), % logging
 
-/*
   atom_concat('fill_full_data_', Object_Type, Fill_Data_Pred),
   Fill_Data_Pred_Arity = 4,
   (
@@ -88,16 +92,15 @@ find(SAT, Actor, Object_Type, Prop_List, DB_Key) :-
 
   write_log('find, after final filter:', [logger(dump_db), lf(1)]),
   dump_db(DB_Key).
-*/
 
 %
-% find(+SAT, +Actor, +Object_Type, +Prop_List, ?Found_Objects)
+
 %
 
-find(SAT, Actor, Object_Type, Prop_List, Found_Objects) :-
+find(SAT, Object_Type, Prop_List, Actor0, Actor. Found_Objects) :-
 
   change_plural(Object_Type, Plural),
   atom_concat('find.', Plural, DB_Key),
   db_clear(DB_Key),
-  find(SAT, Actor, Object_Type, Prop_List, DB_Key), !,
+  find(DB_Key, SAT, Object_Type, Prop_List, Actor0, Actor), !,
   db_to_list(DB_Key, _, Found_Objects).
--- U-238/u/action/templates/find_all_table_rows.pl
+++ U-238/u/action/templates/find_all_table_rows.pl
@@ -1,33 +1,33 @@
-%% This file is a part of Uranium, a general-purpose functional test platform.
-%% Copyright (C) 2011  Sergei Lodyagin
-%%
-%% This library is free software; you can redistribute it and/or
-%% modify it under the terms of the GNU Lesser General Public
-%% License as published by the Free Software Foundation; either
-%% version 2.1 of the License, or (at your option) any later version.
-%% 
-%% This library is distributed in the hope that it will be useful,
-%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-%% Lesser General Public License for more details.
+% This file is a part of Uranium, a general-purpose functional test platform.
+% Copyright (C) 2011  Sergei Lodyagin
+%
+% This library is free software; you can redistribute it and/or
+% modify it under the terms of the GNU Lesser General Public
+% License as published by the Free Software Foundation; either
+% version 2.1 of the License, or (at your option) any later version.
+%
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% Lesser General Public License for more details.
 
-%% You should have received a copy of the GNU Lesser General Public
-%% License along with this library; if not, write to the Free Software
-%% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-%%
-%% e-mail: lodyagin@gmail.com
-%% post:   49017 Ukraine, Dnepropetrovsk per. Kamenski, 6
-%% -------------------------------------------------------------------------------
-%%
+% You should have received a copy of the GNU Lesser General Public
+% License along with this library; if not, write to the Free Software
+% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+%
+% e-mail: lodyagin@gmail.com
+% post:   49017 Ukraine, Dnepropetrovsk per. Kamenski, 6
+% -------------------------------------------------------------------------------
+%
 
 % Template for parsing html tables (row -> object)
 
 :- module(find_all_table_rows, [find_all_table_rows/5]).
 
 :- use_module(u(vd)).
-:- use_module(parser/html/html_page_parse).
-:- use_module(parser/html/html_page_find).
-:- use_module(parser/html/table_v_parse).
+:- use_module(u(parser/html/html_page_parse)).
+:- use_module(u(parser/html/html_page_find)).
+:- use_module(u(parser/html/table_v_parse)).
 
 find_all_table_rows(DB_Key, Page, Table_Search_Columns, Class,
                     Cast_Expr) :-
@@ -38,7 +38,7 @@ find_all_table_rows(DB_Key, Page, Table_Search_Columns, Class,
     html_page_parse(DB2_Key,
                     Page,
                     [table_v]),
-    
+
     dump_db(DB2_Key),
 
     % find tables
@@ -48,7 +48,7 @@ find_all_table_rows(DB_Key, Page, Table_Search_Columns, Class,
                    % treat a table as a matched table
                    % if it contains these columns
                    Table_Search_Columns),
-    
+
     dump_db(DB3_Key),
 
     % concat all found tables and make objects
