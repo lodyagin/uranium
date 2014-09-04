22308bde2c374f0b350462b33a4ff19f0255b410
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-09-04 15:22:03 +0000

    Cosmetic

--- U-238/u/vd.pl
+++ U-238/u/vd.pl
@@ -189,6 +189,8 @@
 :- use_module(u(ur_lists)).
 :- use_module(u(ur_terms)).
 
+:- reexport(u(internal/db_i), [db_key_is_valid/1]).
+
 %% db_clear(+DB_Key) is det.
 %
 % Clear the DB identified by DB_Key if the DB exists.
@@ -1202,79 +1204,74 @@ check_record(\/(Expr1, Expr2), Record) :-
 
 %% db_merge(+DB1_Key, +DB2_Key)
 %
-% try unify DB1_Key x DB2_Key with key restrictions,
+% Try to unify DB1_Key x DB2_Key with key restrictions,
 % leave only unified in DB_Key1,
 % always cast to more narrowed type.
 % Drop all values with incomplete keys.
 % Don't unify if a key is empty (it is a performance restriction).
 %
-/*
 db_merge(DB1_Key, DB2_Key) :-
-
-   Ctx = context(db_merge/3, _),
+   Ctx = context(db_merge/2, _),
    db_merge_cmn(DB1_Key, DB2_Key, default, Ctx).
 
 %% db_merge(+DB1_Key, +DB2_Key, +Key)
 % Merge by Key fields
 
 db_merge(DB1_Key, DB2_Key, Key) :-
-
    Ctx = context(db_merge/3, _),
    db_merge_cmn(DB1_Key, DB2_Key, Key, Ctx).
 
 db_merge_cmn(DB1_Key, DB2_Key, Key, Ctx) :-
-
    check_db_key(DB1_Key, Ctx),
    check_db_key(DB2_Key, Ctx),
-   atom_concat(DB1_Key, '.db_merge', DB_Tmp),
+   atom_concat(DB1_Key, '.#db_merge', DB_Tmp),
+
    write_log(['Start db_merge ', DB1_Key, ' and ', DB2_Key],
              [logger(db_merge), lf(1, before), lf(1)]),
-   (
-   db_recorded_int(DB1_Key, Object1),
-   write_log(['Found', Object1, 'in the first DB'],
-             [logger(db_merge), lf(1, before), lf(1)]),
-   functor(Object1, Class1, _),
-   (  Key = default
-   -> get_key(Class1, Key1)
-   ;  Key1 = Key
-   ),
-   arg(1, Object1, Class1_Id),
-   obj_unify_int(Class1_Id, Key1, throw, Object1, Key1_Value, Ctx),
-   (
-    ground(Key1_Value),
-    write_log(['Got the key value', Key1_Value],
-              [logger(db_merge), lf(1)]),
-    named_args_unify(DB2_Key, Class2, Key1, Key1_Value, Object2)
-    -> %NB use only the first key-unified object from DB2
 
-    write_log(['Found', Object2, 'in the second DB'],
-              [logger(db_merge), lf(1)]),
-    most_narrowed(Class1, Class2, New_Class), - need check with rebasing
-    arg1(1, Object2, Class2_Id),
-    obj_downcast_int(Object1, New_Class, Final_Obj1),
-    obj_downcast_int(Object2, New_Class, Final_Obj2),
+   (  db_recorded_int(DB1_Key, Object1),
 
-    Final_Obj1 = Final_Obj2,
-    db_erase(Object2),
+      write_log(['Found', Object1, 'in the first DB'],
+                [logger(db_merge), lf(1, before), lf(1)]),
 
-    db_recordz(DB_Tmp, Final_Obj2),
-    write_log([Final_Obj2, 'is written into the first DB'],
-              [logger(db_merge), lf(1)])
-    ;
-    write_log('The corresponding object in the second DB is not found',
-              [logger(db_merge), lf(1)])
-   ),
-   fail
-   ;
-   true
+      functor(Object1, Class1, _),
+      (  Key = default
+      -> get_key(Class1, Key1)
+      ;  Key1 = Key
+      ),
+      arg(1, Object1, Class1_Id),
+      obj_unify_int(Class1_Id, Key1, throw, Object1, Key1_Value, Ctx),
+      (
+         ground(Key1_Value),
+
+         write_log(['Got the key value', Key1_Value],
+                   [logger(db_merge), lf(1)]),
+
+         named_args_unify(DB2_Key, Class2, Key1, Key1_Value, Object2)
+      -> %NB use only the first key-unified object from DB2
+
+         write_log(['Found', Object2, 'in the second DB'],
+                   [logger(db_merge), lf(1)]),
+         most_narrowed(Class1, Class2, New_Class), %- need check with rebasing
+         arg1(1, Object2, Class2_Id),
+         obj_downcast_int(Object1, New_Class, Final_Obj1),
+         obj_downcast_int(Object2, New_Class, Final_Obj2),
+
+         Final_Obj1 = Final_Obj2,
+         db_erase(Object2),
+
+         db_recordz(DB_Tmp, Final_Obj2),
+         write_log([Final_Obj2, 'is written into the first DB'],
+                   [logger(db_merge), lf(1)])
+      ;
+         write_log('The corresponding object in the second DB is not found',
+                   [logger(db_merge), lf(1)])
+      ),
+      fail ; true
    ),
    db_clear(DB1_Key),
    db_clear(DB2_Key),
    db_move_all_data(DB_Tmp, DB1_Key).
-
-*/
-
-
 
 
 %% named_args_unify(+DB_Key, ?Functor, +Field_Names, ?Values,
