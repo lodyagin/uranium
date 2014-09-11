46a55c88bf3cfae8953db98dd97f88dee8b65cea
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-09-11 16:52:10 +0000

    Fix in debian package incompatibility with swi-prolog-x

--- debian/uranium/usr/share/uranium/U-238/u/v/test/callup_v.pl
+++
@@ -1,3 +0,0 @@
-:- module(callup_v, []).
-
-new_class(callup_v, citizen_v, [fit_for_military_service]).
--- debian/uranium/usr/share/uranium/U-238/u/v/test/man_v.pl
+++
@@ -1,5 +0,0 @@
-:- module(man_v, []).
-
-new_class(man_v, object_v, [sex, name, surname, weight, height]
-         , [name, surname]
-         ).
