bc55ca57b5f08e8b6f6f6caa85647923844e917e
Sergei Lodyagin <lodyagin@gmail.com>
2015-03-30 11:26:24 +0200

    obj_fill_downcast_random added

--- U-238/u/gt/gt_objects.pl
+++ U-238/u/gt/gt_objects.pl
@@ -1,7 +1,9 @@
 :- module(gt_objects,
-          [obj_fill_random/2]
+          [obj_fill_random/2,
+           obj_fill_downcast_random/3]
          ).
 
+:- use_module(library(assoc)).
 :- use_module(u(internal/check_arg)).
 :- use_module(u(internal/objects_i)).
 :- use_module(u(ur_option)).
@@ -27,3 +29,36 @@ obj_fill_random(Options, Obj) :-
                     obj_field(Obj, Name, Value)
                   ))
           ).
+
+
+is_assoc_fast(t) :- !.
+is_assoc_fast(t(_,_,_,_,_)).
+
+is_list_fast([]) :- !.
+is_list_fast([_|_]).
+
+% Fills then downcasts till no more downcasts
+obj_fill_downcast_random(Options_List, Obj0, Obj) :-
+    is_assoc_fast(Options_List), !,
+    obj_fill_downcast_random_int(Options_List, Obj0, Obj).
+
+obj_fill_downcast_random(Options_List, Obj0, Obj) :-
+    is_list_fast(Options_List), !,
+    list_to_assoc(Options_List, Options_List1),
+    obj_fill_downcast_random_int(Options_List1, Obj0, Obj).
+
+obj_fill_downcast_random_int(Options_List, Obj0, Obj) :-
+    functor(Obj0, New_Class, _),
+    % get options for the new object
+    (  get_assoc(New_Class, Options_List, Options) -> true
+    ;  Options = []
+    ),
+    % fill
+    obj_fill_random(Options, Obj0),
+    % downcast
+    obj_downcast(Obj0, Obj1),
+    (  Obj0 == Obj1
+    -> Obj = Obj1  % no more downcasts
+    ;  obj_fill_downcast_random_int(Options_List, Obj1, Obj)
+    ).
+
