77ea425e9124b96859fc80d19308db8d9f9d1147
Sergei Lodyagin <lodyagin@gmail.com>
2015-04-29 15:27:22 +0200

    propagate/4

--- U-238/u/ur_matrix.pl
+++ U-238/u/ur_matrix.pl
@@ -1,6 +1,7 @@
 :- module(ur_matrix,
-          [unify/2,    % +M1, +M2
-           propagate/3   % +Vector, +Multiply, -Matrix
+          [unify/2,      % +M1, +M2
+           propagate/3,  % +Vector, +Multiply, -Matrix
+           propagate/4   % :Copy, +Vector, +Multiply, -Matrix
           ]).
 
 :- use_module(library(error)).
@@ -17,21 +18,36 @@ propagate(Vector, Multiply, Matrix) :-
    Ctx = context(propagate/3, _),
    check_inst(Vector, Ctx),
    must_be(nonneg, Multiply),
-   propagate_cmn(Vector, Multiply, Matrix).
+   propagate_cmn((=), Vector, Multiply, Matrix).
 
-propagate_cmn([], _, []) :- !.
-propagate_cmn(Vector, Multiply, Matrix) :-
+:- meta_predicate propagate(2, +, +, -).
+
+%% propagate(+Copy, +Vector, +Multiply, -Matrix) is det.
+%
+% It is like propagate/3 but uses Copy for make copy Vector-s.
+%
+propagate(Copy, Vector, Multiply, Matrix) :-
+   Ctx = context(propagate/4, _),
+   check_inst(Vector, Ctx),
+   must_be(nonneg, Multiply),
+   must_be(callable, Copy),
+   propagate_cmn(Copy, Vector, Multiply, Matrix).
+   
+
+propagate_cmn(_, [], _, []) :- !.
+propagate_cmn(Copy, Vector, Multiply, Matrix) :-
    Vector = [[_] | _], !,
    transpose(Vector, [Vector1]),
-   propagate_vertically(Vector1, Multiply, [], Matrix1),
+   propagate_vertically(Copy, Vector1, Multiply, [], Matrix1),
    transpose(Matrix1, Matrix).
-propagate_cmn(Vector, Multiply, Matrix) :-
-   propagate_vertically(Vector, Multiply, [], Matrix).
+propagate_cmn(Copy, Vector, Multiply, Matrix) :-
+   propagate_vertically(Copy, Vector, Multiply, [], Matrix).
 
-propagate_vertically(_, 0, M, M) :- !.
-propagate_vertically(V, K, M0, M) :-
+propagate_vertically(_, _, 0, M, M) :- !.
+propagate_vertically(Copy, V, K, M0, M) :-
    succ(K1, K),
-   propagate_vertically(V, K1, [V|M0], M).
+   call(Copy, V, V0),
+   propagate_vertically(Copy, V, K1, [V0|M0], M).
 
 
 
