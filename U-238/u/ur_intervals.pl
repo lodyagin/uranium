b31d3776a47039ec8df3b252c881164b6f9b49f1
Sergei Lodyagin <lodyagin@gmail.com>
2015-05-22 22:17:45 +0200

    dom_nth{0,1}/2

--- U-238/u/ur_intervals.pl
+++ U-238/u/ur_intervals.pl
@@ -3,9 +3,11 @@
 
 :- module(ur_intervals,
           [dom_list/2, % ?Dom, ?List
-           dom_nth1/2  % ?Idx, +Dom, ?N
+           dom_nth0/3, % ?Idx, +Dom, ?N
+           dom_nth1/3  % ?Idx, +Dom, ?N
           ]).
 
+:- use_module(library(error)).
 :- use_module(library(clpfd)).
 
 %% dom_list(?Dom, ?List) is det.
@@ -32,7 +34,6 @@ list_to_dom([B0|T], Dom0, Dom) :-
   interval_to_number(B0, B),
   list_to_dom(T, \/(Dom0, B), Dom).
   
-
 dom_to_list(\/(Head, Last0), List, Tail) :- !,
   number_to_interval(Last0, Last),
   dom_to_list(Head, List, [Last|Tail]).
@@ -45,7 +46,38 @@ number_to_interval(A, A..A).
 interval_to_number(A..A, A) :- !.
 interval_to_number(A..B, A..B).
 
+%% dom_nth0(?Idx, +Dom, ?N)
+%
+dom_nth0(Idx, Dom, N) :-
+  Ctx = context(dom_nth0/3, _),
+  dom_nth0_cmn(Idx, Dom, N, Ctx).
+
 %% dom_nth1(?Idx, +Dom, ?N)
 %
-%dom_nth1(Idx, Dom, N) :-
-  
+dom_nth1(Idx, Dom, N) :-
+  Ctx = context(dom_nth0/3, _),
+  Idx0 #= Idx - 1,
+  dom_nth0_cmn(Idx0, Dom, N, Ctx).
+
+dom_nth0_cmn(_, Dom, _, Ctx) :-
+  var(Dom), !,
+  throw(error(instantiation_error, Ctx)).
+dom_nth0_cmn(Idx, _, _, Ctx) :-
+  var(Idx), !,
+  throw(error(not_implemented, Ctx)).
+dom_nth0_cmn(Idx, Dom, N, Ctx) :-
+  must_be(nonneg, Idx),
+  (  dom_list(Dom, List)
+  -> dom_nth0_int(Idx, List, N)
+  ;  throw(error(domain_error(fd_domain, Dom), Ctx))
+  ).
+
+dom_nth0_int(_, [], _) :- !, fail.
+dom_nth0_int(K, [N..M|T], El) :-
+  L is M - N + 1,
+  (  K < L
+  -> El is N + K
+  ;  K1 is K - L,
+     dom_nth0_int(K1, T, El)
+  ).
+
--- U-238/u/ur_intervals.plt
+++ U-238/u/ur_intervals.plt
@@ -3,10 +3,44 @@
 :- use_module(library(clpfd)).
 
 test(dom_list1) :-
-  A in 1..10, A#\=3, A#\=6, A#\=9, fd_dom(A, Dom1),
+  dom1(Dom1),
   dom_list(Dom1, List1),
   dom_list(Dom2, List1),
   assertion(List1 == [1..2, 4..5, 7..8, 10..10]),
   assertion(Dom1 == Dom2).
 
+test(dom_list2) :-
+  A = 1, fd_dom(A, Dom1),
+  dom_list(Dom1, List1),
+  dom_list(Dom2, List1),
+  assertion(List1 == [1..1]),
+  assertion(Dom1 == Dom2).
+
+test(dom_nth0) :-
+  dom1(Dom1),
+  findall( El..El,
+           ( between(0, 6, Idx),
+             dom_nth0(Idx, Dom1, El) ),
+           List
+         ),
+  dom_list(Dom2, List),
+  A in Dom2,
+  fd_dom(A, Dom3),
+  assertion(Dom1 == Dom3).
+  
+test(dom_nth1) :-
+  dom1(Dom1),
+  findall( El..El,
+           ( between(1, 7, Idx),
+             dom_nth1(Idx, Dom1, El) ),
+           List
+         ),
+  dom_list(Dom2, List),
+  A in Dom2,
+  fd_dom(A, Dom3),
+  assertion(Dom1 == Dom3).
+  
+dom1(Dom) :- 
+  A in 1..10, A#\=3, A#\=6, A#\=9, fd_dom(A, Dom).
+  
 :- end_tests(ur_intervals).
