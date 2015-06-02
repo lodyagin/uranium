9a26488f40a7d4184589ab1b935043807244d041
Sergei Lodyagin <lodyagin@gmail.com>
2015-06-02 18:44:31 +0200

    wip

--- U-238/u/ur_intervals.pl
+++ U-238/u/ur_intervals.pl
@@ -1,14 +1,44 @@
-% Operating in CPL(FD) domains as intervals.
+% Operating on intervals.
+/*
+   An intervals in Uranium are represented as trees. Each node is one of:
+
+   empty: empty interval.
+
+   split(N, Left, Right, Size)
+      - split on integer N, with Left and Right domains whose elements are
+        all less than and greater than N, respectively. The domain is the
+        union of Left and Right, i.e., N is a hole. Size is a size
+        of the interval.
+
+   from_to(From, To)
+      - interval (From-1, To+1); From and To are bounds
+*/
 % @author Sergei Lodyagin
 
 :- module(ur_intervals,
           [dom_member/2, % ?El, +Dom
            dom_nth0/3, % ?Idx, +Dom, ?N
-           dom_nth1/3  % ?Idx, +Dom, ?N
+           dom_nth1/3,  % ?Idx, +Dom, ?N
+           interval_size  % +Interval, -Size
           ]).
 
 :- use_module(library(error)).
 :- use_module(library(clpfd)).
+
+%% intervals_size(+Intervals, -Size) is det.
+%
+% Size is a number of elements in Intervals.
+%
+intervals_size(Intervals, Size) :-
+  var(Intervals), !,
+  instantiation_error(Intervals).
+intervals_size(split(_, _, _, Size), Size) :- !.
+intervals_size(empty, 0) :- !.
+intervals_size(from_to(From, To), Size) :- !,
+  Size is To - From + 1,
+  Size > 0.
+intervals_size(Term, _) :-
+  domain_error(uranium_intervals, Term).
 
 %% dom_member(?El, +Dom) is nondet.
 %
@@ -16,6 +46,13 @@ dom_member(El, Dom) :-
   Ctx = context(dom_member/2, _),
   any_to_intervals(Dom, Intervals, Ctx),
   dom_member_int(El, Intervals).
+
+dom_member_int(Y, from_to(From, To)) :- !,
+  between(From, To, Y).
+dom_member_int(Y, split(_, Left, Right, _)) :-
+  (  dom_member_int(Y, Left)
+  ;  dom_member_int(Y, Right)
+  ).
 
 dom_member_int(Y, [n(From)-n(To)|T]) :-
   (  between(From, To, Y)
