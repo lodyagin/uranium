e4c8a7ce57e301497635640e9822c9adbea3af7a
Sergei Lodyagin <lodyagin@gmail.com>
2016-04-28 01:04:58 +0200

    dijkstra_algo_test example wip

--- U-238/examples/dijkstra_algo_test/test.pl
+++ U-238/examples/dijkstra_algo_test/test.pl
@@ -1,4 +1,14 @@
 :- use_module(library(error)).
 
 all_branches(N_Nodes, Branches) :-
-   findall(A-B, ( between(1, N_Nodes, A), succ(A, A1), between(A1, N_Nodes, B) ), Branches).
+   findall(A-B,
+           ( between(1, N_Nodes, A),
+             succ(A, A1),
+             between(A1, N_Nodes, B) ),
+           Branches).
+
+random_options(I,
+               [generator(randgen:pcg32_1),
+                rand_state(pcg32_init(I, 1))
+               ]).
+   
