7facfc14ab27fef5aa33e1142f827e3ce7df971c
Sergei Lodyagin <lodyagin@gmail.com>
2016-09-16 13:36:27 +0200

    odbc extensions

--- U-238/u/db/v/odbc_table_v.pl
+++ U-238/u/db/v/odbc_table_v.pl
@@ -3,3 +3,7 @@
           ]).
 
 new_class(odbc_table_v, db_object_v, []).
+
+'odbc_table_v?'(Obj, schema, Schema) :-
+    obj_field(Obj, db_key, ODBC),
+    ( nonvar(ODBC), functor(ODBC, odbc, _) -> arg(2, ODBC, Schema) ; true).
